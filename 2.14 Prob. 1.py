c83a47d5dd48b460a9a44b63a3bf86720b3a836d
Deagan2nd <drherrera@mavs.coloradomesa.edu>
2025-09-02 14:09:22 -0600

    Add files via upload

---
+++ 2.14 Prob. 1.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        2.14 Prob. 1
+# Purpose:
+#
+# Author:      drherrera
+#
+# Created:     9/2/2025
+# Copyright:   (c) drherrera 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+Word1= ("All")
+
+Word2= ("Work")
+
+Word3= ("And")
+
+Word4= ("No")
+
+Word5= ("Play")
+
+Word6= ("Makes")
+
+Word7= ("Jack")
+
+Word8= ("A")
+
+Word9= ("Dull")
+
+Word10= ("Boy")
+
+
+print (Word1, Word2, Word3, Word4, Word5, Word6, Word7, Word8, Word9, Word10)
